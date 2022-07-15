1cd249ecc9a6cbf2b6b9123873b0cd8cb40ce591
Ernesto <48799528+ernestod10@users.noreply.github.com>
2022-07-14 21:12:47 -0400

    Update triggers&programs.sql

--- triggers&programs.sql
+++ triggers&programs.sql
@@ -1,4 +1,4 @@
---trigger where votos_postular_id can't repeat more than 3 times 
+
 Create or replace TRIGGER "votos_postular_trigger" BEFORE INSERT ON "votos_postular"
 FOR EACH ROW
 BEGIN
@@ -23,55 +23,34 @@ BEGIN
     (SELECT COUNT(*) FROM Votos WHERE miembro_academia_id_miembro = NEW.miembro_academia_id_miembro) > 1 AND;
 END;
 
---trigger where votos_postular_id can't repeat more than 3 times 
+
 
 
 -- Procedures
 
 --counts the number of times pelicula_postulada_id_pelicula repeats from votos_postular and inserts the top 35 into postulacion
 Create or replace PROCEDURE contar_nominacion (year Integer )
+AS $$
+    DECLARE
+        pelicula_postulada_id_pelicula Integer;
+        votos_postular_id_voto_postulado Integer;
+        votos_postular_id_voto_postulado_count Integer;
+        votos_postular_id_voto_postulado_count_max Integer;
+
     BEGIN
-        DECLARE votos_postular_id_pelicula_count INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_2 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_3 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_4 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_5 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_6 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_7 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_8 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_9 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_10 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_11 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_12 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_13 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_14 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_15 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_16 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_17 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_18 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_19 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_20 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_21 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_22 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_23 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_24 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_25 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_26 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_27 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_28 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_29 INTEGER;
-        DECLARE votos_postular_id_pelicula_count_temp_30 INTEGER;
-            
-END
+        FOR pelicula_postulada_id_pelicula IN (SELECT pelicula_postulada_id_pelicula FROM pelicula_postulada) LOOP
+            votos_postular_id_voto_postulado_count_max := 0;
+            FOR votos_postular_id_voto_postulado IN (SELECT votos_postular_id_voto_postulado FROM votos_postular WHERE pelicula_postulada_id_pelicula = pelicula_postulada_id_pelicula) LOOP
+                votos_postular_id_voto_postulado_count := (SELECT COUNT(*) FROM votos_postular WHERE votos_postular_id_voto_postulado = votos_postular_id_voto_postulado);
+                IF votos_postular_id_voto_postulado_count > votos_postular_id_voto_postulado_count_max THEN
+                    votos_postular_id_voto_postulado_count_max := votos_postular_id_voto_postulado_count;
+                END IF;
+            END LOOP;
+            INSERT INTO postulacion (pelicula_postulada_id_pelicula, votos_postular_id_voto_postulado_count) VALUES (pelicula_postulada_id_pelicula, votos_postular_id_voto_postulado_count_max);
+        END LOOP;
+    END;
 
-
-        
-
-
-        
-
-
+-- counts the top 
 
 
 
